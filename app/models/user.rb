b3543e4727ffd796ced5d34887382935eccf0d77
Misha Ilchyshyn <ilchyshynmuchailo@gmail.com>
2025-10-15 16:35:16 +0300

    Company has many users and quotes associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable, :rememberable, :registerable
 
   devise :database_authenticatable, :validatable
+
+  belongs_to :company
 end
